92eb88dc2d65c6c04351566c0520b04e6db301e0
chinadarj <chinadarj@gmail.com>
2025-08-03 19:58:38 +0530

    removed front end folder

--- frontend/src/services/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const API_BASE = 'https://gaqsyyz9t5.execute-api.ap-south-1.amazonaws.com';
-
-const api = axios.create({
-  baseURL: API_BASE,
-});
-
-export default api;
